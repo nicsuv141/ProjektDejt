5586ea121319c941ceb3111699ade6faa0f51586
nicsuv141 <nicsuv141@studentmail.oru.se>
2016-12-20 14:40:58 +0100

    Models

--- DejtProjekt/DejtProjekt/Controllers/HomeController.cs
+++ DejtProjekt/DejtProjekt/Controllers/HomeController.cs
@@ -12,7 +12,7 @@ namespace DejtProjekt.Controllers
         {
             return View();
         }
-        
+        [Authorize]
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
@@ -20,6 +20,7 @@ namespace DejtProjekt.Controllers
             return View();
         }
 
+        [Authorize]
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
---
+++ DejtProjekt/DejtProjekt/Models/UserModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DejtProjekt.Models
+{
+    //[Bind(Include = "Username, Password,FirstName,LastName,Email,Hidden, Image, Gender,Phone,Country,LookingFor")]
+    public class UserModel
+    {
+        public int UserId { get; set; }  // Primary key
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string UserName { get; set; }
+        public char? Hidden { get; set; }
+        public byte?[] Image { get; set; }
+        public bool? Gender { get; set; }
+        public string Phone { get; set; }
+        public string Country { get; set; }
+        public char? LookingFor { get; set; }
+        //public int? PostId { get; set; }  // Foreign key 
+        //public int? FriendId { get; set; } // Foreign entity
+    }
+}
