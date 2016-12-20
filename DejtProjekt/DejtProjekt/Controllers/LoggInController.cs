95cf5fb5a56d1e4a59011faa4f23060a1cd8f6ad
Alexander Jansson <Alexanderjan94@gmail.com>
2016-12-20 14:41:34 +0100

    f

---
+++ DejtProjekt/DejtProjekt/Controllers/LoggInController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DejtProjekt.Controllers
+{
+    public class LoggInController : Controller
+    {
+        // GET: LoggIn
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- DejtProjekt/DejtProjekt/Models/IdentityModels.cs
+++ DejtProjekt/DejtProjekt/Models/IdentityModels.cs
@@ -9,6 +9,9 @@ namespace DejtProjekt.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
@@ -28,6 +31,13 @@ namespace DejtProjekt.Models
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<ApplicationUser>().ToTable("Account").Property(p=>p.Id).HasColumnName("AccountID"));
+            modelBuilder.Entity<IdentityUserClaim>().ToTable("userClaim");
         }
     }
 }
