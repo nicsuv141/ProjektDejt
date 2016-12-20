1f3c2cdbc7e072428b3e54b45727f17262baaa2e
nicsuv141 <nicsuv141@studentmail.oru.se>
2016-12-20 14:51:52 +0100

    Syncsync

--- DejtProjekt/DejtProjekt/Models/UserModel.cs
+++ DejtProjekt/DejtProjekt/Models/UserModel.cs
@@ -2,19 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
 
 namespace DejtProjekt.Models
 {
-    //[Bind(Include = "Username, Password,FirstName,LastName,Email,Hidden, Image, Gender,Phone,Country,LookingFor")]
+    [Bind(Include = "Username, Password,FirstName,LastName,Email,Hidden, Image, Gender,Phone,Country,LookingFor")]
     public class UserModel
     {
+        [Key]
         public int UserId { get; set; }  // Primary key
+        [Required(ErrorMessage = "Skriv in ett användarnamn.")]
         public string Username { get; set; }
+        [Required(ErrorMessage = "Skriv in ett lösenord.")]
         public string Password { get; set; }
+        [Required(ErrorMessage = "Skriv in ditt förnamn.")]
         public string FirstName { get; set; }
+        [Required(ErrorMessage = "Skriv in ditt efternamn")]
         public string LastName { get; set; }
+        [Required(ErrorMessage = "Skriv in en mejladress.")]
         public string Email { get; set; }
-        public string UserName { get; set; }
+        //public string UserName { get; set; }
         public char? Hidden { get; set; }
         public byte?[] Image { get; set; }
         public bool? Gender { get; set; }
