8d86f77f334c977c20010d0806534137a2641653
nicsuv141 <nicsuv141@studentmail.oru.se>
2016-12-21 16:41:56 +0100

    asd

--- DejtProjekt/DejtProjekt/Models/OurDbInitializer.cs
+++ DejtProjekt/DejtProjekt/Models/OurDbInitializer.cs
@@ -43,6 +43,37 @@ namespace DejtProjekt.Models
             });
             base.Seed(context);
 
+            context.Files.Add(new File
+            {
+                FileName = "trump.png",
+                ContentType = "image/png",
+                Content = (byte)0x89504E470D0A1A0A0000000D49484452000000F0000000F00802000000B1377EC5000000017352474200AECE1CE90000000467414D410000B18F0BFC6105000000097048597300000EC300000EC301C76FA8640000FFA549444154785E74FD657424679AAE8DFACF396B9FD93DD3DDA662103333670A1294CCCCCC2CA532A554,
+                FileType = (FileType)1,
+                UserId = 1
+
+            });
+
+            context.Files.Add(new File
+            {
+                FileName = "putin.png",
+                ContentType = "image/png",
+                Content = 0x89504E470D0A1A0A0000000D494844520000019E0000022508020000006AFB0973000000017352474200AECE1CE90000000467414D410000B18F0BFC6105000000097048597300000EC300000EC301C76FA8640000FFA549444154785E7CFDF7971457BEED8B964BEFBDF7DEDBC8309919E9BD2B9FE53D557810C80B100204C2,
+                FileType = (FileType)1,
+                UserId = 2
+
+            });
+
+            context.Files.Add(new File
+            {
+                FileName = "lofven.png",
+                ContentType = "image/png",
+                Content = 0x89504E470D0A1A0A0000000D4948445200000104000001450802000000856E0494000000017352474200AECE1CE90000000467414D410000B18F0BFC6105000000097048597300000EC300000EC301C76FA8640000FFA549444154785EECFD77B024497A1F086668953AF36951BABABB5ACFF4E801663083D118689030E3AE2D,
+                FileType = (FileType)1,
+                UserId = 3
+
+            });
         }
+
+        
     }
 }
