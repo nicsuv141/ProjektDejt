6d7c2dcbdadd71b078659973af8ad974f6f07312
nicsuv141 <nicsuv141@studentmail.oru.se>
2016-12-29 13:20:32 +0100

    lhj

--- DejtProjekt/DejtProjekt/Controllers/LoggInController.cs
+++ DejtProjekt/DejtProjekt/Controllers/LoggInController.cs
@@ -69,87 +69,87 @@ namespace DejtProjekt.Controllers
         }
 
         //Log
-        //public ActionResult Login()
-        //{
-        //    return View();
-        //}
-
-        //[HttpPost]
-
-        //public ActionResult Login(UserModel user)
-        //{
-        //    try
-        //    {
-        //        using (OurDbContext db = new OurDbContext())
-        //        {
-        //            var usr = db.userModel.Where(u => u.Username == user.Username && u.NewPassword == user.NewPassword).FirstOrDefault();
-        //            if (usr != null)
-        //            {
-        //                Session["UserID"] = usr.UserID.ToString();
-        //                Session["Username"] = usr.Username.ToString();
-        //                return RedirectToAction("LoggedIn");
-        //            }
-        //            else
-        //            {
-        //                ModelState.AddModelError("", "Username or Password is wrong");
-        //            }
-        //        }
-        //    }
-        //    catch (Exception e)
-        //    {
-        //        Console.WriteLine(e);
-        //    }
-        //    return View();
-        //}
-
-        [HttpGet]
-        public ActionResult Login(string returnUrl)
+        public ActionResult Login()
         {
-            var model = new UserModel
-            {
-                ReturnUrl = returnUrl
-            };
-
-            return View(model);
+            return View();
         }
 
         [HttpPost]
-        public ActionResult LogIn(UserModel model)
+
+        public ActionResult Login(UserModel user)
         {
-            if (!ModelState.IsValid) //Checks if input fields have the correct format
+            try
             {
-                return View(model); //Returns the view with the input values so that the user doesn't have to retype again
+                using (OurDbContext db = new OurDbContext())
+                {
+                    var usr = db.userModel.Where(u => u.Username == user.Username && u.NewPassword == user.NewPassword).FirstOrDefault();
+                    if (usr != null)
+                    {
+                        Session["UserID"] = usr.UserID.ToString();
+                        Session["Username"] = usr.Username.ToString();
+                        return RedirectToAction("LoggedIn");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "Username or Password is wrong");
+                    }
+                }
             }
-
-            //Checks whether the input is the same as those literals. Note: Never ever do this! This is just to demo the validation while we're not yet doing any database interaction
-            if (model.Email == "admin@admin.com" & model.NewPassword == "123456")
-        {
-                var identity = new ClaimsIdentity(new[] {
-                new Claim(ClaimTypes.Name, "Xtian"),
-                new Claim(ClaimTypes.Email, "xtian@email.com"),
-                new Claim(ClaimTypes.Country, "Philippines")
-        }, 
-            "ApplicationCookie");
-
-            var ctx = Request.GetOwinContext();
-            var authManager = ctx.Authentication;
-            authManager.SignIn(identity);
-
-            return Redirect(GetRedirectUrl(model.ReturnUrl));
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            return View();
         }
 
-            ModelState.AddModelError("", "Invalid email or password");
-            return View(model);
-    }
+        //    [HttpGet]
+        //    public ActionResult Login(string returnUrl)
+        //    {
+        //        var model = new UserModel
+        //        {
+        //            ReturnUrl = returnUrl
+        //        };
 
-        private string GetRedirectUrl(string returnUrl)
-        {
-            if (string.IsNullOrEmpty(returnUrl) || !Url.IsLocalUrl(returnUrl))
-            {
-                return Url.Action("index", "home");
-            }
-            return returnUrl;
-        }
+        //        return View(model);
+        //    }
+
+        //    [HttpPost]
+        //    public ActionResult LogIn(UserModel model)
+        //    {
+        //        if (!ModelState.IsValid) //Checks if input fields have the correct format
+        //        {
+        //            return View(model); //Returns the view with the input values so that the user doesn't have to retype again
+        //        }
+
+        //        //Checks whether the input is the same as those literals. Note: Never ever do this! This is just to demo the validation while we're not yet doing any database interaction
+        //        if (model.Email == "admin@admin.com" & model.NewPassword == "123456")
+        //    {
+        //            var identity = new ClaimsIdentity(new[] {
+        //            new Claim(ClaimTypes.Name, "Xtian"),
+        //            new Claim(ClaimTypes.Email, "xtian@email.com"),
+        //            new Claim(ClaimTypes.Country, "Philippines")
+        //    }, 
+        //        "ApplicationCookie");
+
+        //        var ctx = Request.GetOwinContext();
+        //        var authManager = ctx.Authentication;
+        //        authManager.SignIn(identity);
+
+        //        return Redirect(GetRedirectUrl(model.ReturnUrl));
+        //    }
+
+        //        ModelState.AddModelError("", "Invalid email or password");
+        //        return View(model);
+        //}
+
+        //    private string GetRedirectUrl(string returnUrl)
+        //    {
+        //        if (string.IsNullOrEmpty(returnUrl) || !Url.IsLocalUrl(returnUrl))
+        //        {
+        //            return Url.Action("index", "home");
+        //        }
+        //        return returnUrl;
+        //    }
 
 
         public ActionResult LoggedIn()
