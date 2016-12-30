f060e8bc6bc2c75591ec8810f77ddc5628104308
nicsuv141 <nicsuv141@studentmail.oru.se>
2016-12-30 14:49:34 +0100

    ret

---
+++ DejtProjekt/DejtProjekt/Models/Friends.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DejtProjekt.Models
+{
+    public class Friends
+    {
+        public int UserId { get; set; }
+        public int FriendId { get; set; }
+
+        public virtual UserModel User { get; set; }
+        public virtual UserModel Friend { get; set; }
+
+        public virtual ICollection<UserModel> Users { get; set; }
+    }
+}
